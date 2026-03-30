3e2e78b63abb55c002c19dcb991ba8ee6c63b999
fakhri-rasyad <kaizerd23@gmail.com>
2026-03-30 21:49:41 +0800

    feat: add database migrations for user

--- database/migrations/000001_create_users_table.up.sql
+++ database/migrations/000001_create_users_table.up.sql
@@ -1,16 +1,15 @@
 CREATE EXTENSION
 IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE users
-(
+CREATE TABLE users (
     internal_id BIGSERIAL PRIMARY KEY,
-    public_id UUID NOT NULL DEFAULT gen_random_uuid()
+    public_id UUID NOT NULL DEFAULT gen_random_uuid(),
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password text NOT NULL,
-    role VARCHAR(50) NOT NULL DEFAULT 'user',
+    role VARCHAR(15) NOT NULL DEFAULT 'user',
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMPTZ NULL
+    deleted_at TIMESTAMPTZ NULL,
     CONSTRAINT user_public_id_unique UNIQUE(public_id)
 );
